db2d766323ac8a75611612f3ccb22eb70d3666e0
parogers <peter.rogers@gmail.com>
2014-11-16 01:21:02 -0500

    Cleaned up range finder code (added documentation)

--- examples/rangefinder/ranger.py
+++ examples/rangefinder/ranger.py
@@ -2,41 +2,41 @@
 
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
 
-TRIG = 3
 ECHO = 2
+TRIG = 3
 
-#print "Distance Measurement In Progress"
-
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
 GPIO.output(TRIG, False)
-#print "Waiting For Sensor To Settle"
 time.sleep(1)
 
 while 1:
+  # The module wants 10us to send the ultra-sonic pulses
   GPIO.output(TRIG, True)
   time.sleep(0.00001)
   GPIO.output(TRIG, False)
 
+  # The module sends back a high pulse that times how long it took
+  # to receive an ultrasound signal back. So wait until the ECHO
+  # pin goes high (start of the pulse) and note the time.
   pulse_start = 0
   pulse_end = 0
   start = time.time()
-  while GPIO.input(ECHO)==0 and time.time()-start < 2:
+  while not(GPIO.input(ECHO) and time.time()-start < 2):
     pulse_start = time.time()
 
-  while GPIO.input(ECHO)==1:
+  # Wait for the pulse to go low again
+  while (GPIO.input(ECHO)):
     pulse_end = time.time()
 
+  # Calculate the duration, and use that 
   pulse_duration = pulse_end - pulse_start
-
   distance = pulse_duration * 17150
 
-  distance = round(distance, 2)
-
-  print "Distance:",distance,"cm"
+  print "Distance: %0.1f cm" % distance
 
 GPIO.cleanup()
 
